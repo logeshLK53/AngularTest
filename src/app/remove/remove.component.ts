f2b908c5866e19ae64563410b38b11f5638f410d
logeshLK53 <101236953+logeshLK53@users.noreply.github.com>
2022-05-19 18:23:21 +0530

    Add files via upload
    AngularTest

---
+++ src/app/remove/remove.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { LoanService } from '../loan.service';
+@Component({
+  selector: 'app-remove',
+  templateUrl: './remove.component.html',
+  styleUrls: ['./remove.component.css']
+})
+export class RemoveComponent implements OnInit {
+
+  constructor(private ls:LoanService) { }
+
+  ngOnInit() {
+  }
+  remove(loanId){
+    this.ls.removeLoan(loanId);
+      }
+
+
+}
